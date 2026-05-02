d5c0b4015cb2af9c54b82250189e8173f8fc63a6
nicolasfranco777 <nf702136@gmail.com>
2026-05-02 09:42:05 -0500

    Implement main method for circular queue operations

--- pc2.java
+++ pc2.java
@@ -39,3 +39,63 @@ class ColaCircular {
         System.out.println();
     }
 }
+
+
+//main
+
+package javaapplication2;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        ColaCircular cola = new ColaCircular(5);
+        int opcion;
+
+        do {
+            opcion = mostrarMenu(
+                    "COLA CIRCULAR",
+                    "Colar",
+                    "Desencolar",
+                    "Mostrar",
+                    "Salir"
+            );
+
+            switch (opcion) {
+                case 1 -> {
+                    System.out.print("Valor: ");
+                    int valor = sc.nextInt();
+                    cola.enqueue(valor);
+                }
+
+                case 2 -> {
+            int eliminado = cola.dequeue();
+
+            if (eliminado == -1) {
+                System.out.println("No hay valores en la cola");
+            } else {
+                System.out.println("Eliminado: " + eliminado);
+            }
+        }
+
+                case 3 -> cola.mostrar();
+
+            }
+
+        } while (opcion != 4);
+    }
+
+    public static int mostrarMenu(String titulo, String... opciones) {
+        System.out.println("\n=== " + titulo + " ===");
+
+        for (int i = 0; i < opciones.length; i++) {
+            System.out.println((i + 1) + ". " + opciones[i]);
+        }
+
+        System.out.print("Opción: ");
+        return sc.nextInt();
+    }
+}
